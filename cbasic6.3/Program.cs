aeaf905bfe3d25f9e5795e017588750114083fff
TechJerryyy <msohagia@aakashinformatics.com>
2023-02-15 16:58:08 +0530

    Add project files.

---
+++ cbasic6.3/Program.cs
@@ -0,0 +1,35 @@
+﻿/*3 - Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try.
+	Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace cbasic6._3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+			
+			var list = new List<int>();
+
+			while (list.Count < 5)
+			{
+				Console.WriteLine("Type 5 unique numbers:");
+				var input = Convert.ToInt32(Console.ReadLine());
+
+				if (list.Contains(input))
+					Console.WriteLine("Error! Number already in list!");
+				else
+					list.Add(input);
+			}
+
+			list.Sort();
+			foreach (var number in list)
+			{
+				Console.Write("{0} ", number);
+			}
+		}
+    }
+}
